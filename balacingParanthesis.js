24c979077d54264cd257e4c76da006c8a665a4a8
MOHAMED NAUFAL <mnaufalmz@gmail.com>
2022-07-19 17:41:18 +0530

    commit

---
+++ balacingParanthesis.js
@@ -0,0 +1,37 @@
+// Practice - Stack and Queue
+
+let expr = "([{}])";
+
+function areBracketsBalanced(expr) {
+  let stack = [];
+  for (let i = 0; i < expr.length; i++) {
+    let x = expr[i];
+    if (x == "(" || x == "{" || x == "[") {
+      stack.push(x);
+      continue;
+    }
+    if (stack.length == 0) return false;
+
+    let check;
+    switch (x) {
+      case ")":
+        check = stack.pop();
+        if (check == "{" || check == "[") return false;
+        break;
+      case "}":
+        check = stack.pop();
+        if (check == "(" || check == "[") return false;
+        break;
+      case "]":
+        check = stack.pop();
+        if (check == "{" || check == "(") return false;
+        break;
+    }
+  }
+  return stack.length == 0;
+}
+
+//Function Call
+
+if (areBracketsBalanced(expr)) console.log("Balanced");
+else console.log("Unbalanced");
